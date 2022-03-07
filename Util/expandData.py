8e61631546d09f902faa0866dcd113dc261730a3
HenryI259 <hison259@gmail.com>
2022-03-06 23:23:07 -0600

    fixed data files

--- Util/expandData.py
+++ Util/expandData.py
@@ -2,9 +2,11 @@ import pickle
 import gzip
 import os.path
 import random
+import numpy as np
 from helperFunctions import *
+from mnistLoader import *
 
-def expandData(amount=1):
+def expandData():
     if os.path.exists('Data/mnist_expanded.pkl.gz'):
         print('This data already exists')
     else:
@@ -16,32 +18,22 @@ def expandData(amount=1):
         expandedDataPairs = []
         for input, output in zip(trainingData[0], trainingData[1]):
             expandedDataPairs.append((input, output))
+            image = np.reshape(input, (-1, 28))
             counter += 1
             if counter % 1000 == 0:
                 print("Expanding image " + str(counter))
 
-            inputReformat = []
-            inputRow = []
-            for i in input:
-                inputRow.append(i)
-                if len(inputRow) == 28:
-                    inputReformat.append(inputRow)
-                    inputRow = []
-            for i in range(amount):
-                for xy, shiftBy in [('x', 1), ('x', -1), ('y', 1), ('y', -1)]:
-                    if xy == 'x':
-                        newImage = []
-                        for row in inputReformat:
-                            newImage.append(shift(row, shiftBy * (i+1)))
-                    elif xy == 'y':
-                        newImage = shift(inputReformat, shiftBy * (i+1))
-
-                    newData = []
-                    for row in newImage:
-                        for item in row:
-                            newData.append(item)
-
-                    expandedDataPairs.append((newData, output))
+            for d, axis, index_position, index in [
+            (1,  0, "first", 0),
+            (-1, 0, "first", 27),
+            (1,  1, "last",  0),
+            (-1, 1, "last",  27)]:
+                new_img = np.roll(image, d, axis)
+                if index_position == "first": 
+                    new_img[index, :] = np.zeros(28)
+                else: 
+                    new_img[:, index] = np.zeros(28)
+                    expandedDataPairs.append((np.reshape(new_img, 784), output))
 
         random.shuffle(expandedDataPairs)
         expandedTrainingData = [list(d) for d in zip(*expandedDataPairs)]
@@ -50,9 +42,4 @@ def expandData(amount=1):
         pickle.dump((expandedTrainingData, validationData, testData), f)
         f.close()
 
-#expandData()
-print('done')
-f = gzip.open('Data/expanded_mnist.pkl.gz', 'rb')
-trainingData, validationData, testData = pickle.load(f, encoding='latin1')
-f.close()
-print(len(trainingData))
+expandData()
--- Util/mnistLoader.py
+++ Util/mnistLoader.py
@@ -1,15 +1,24 @@
 import gzip
 import pickle
-from helperFunctions import vectorResult
+import numpy as np
 
 def loadData():
-    f = gzip.open('mnist.pkl.gz', 'rb')
+    f = gzip.open('Data/mnist.pkl.gz', 'rb')
     trainingData, validationData, testData = pickle.load(f, encoding='latin1')
     return (trainingData, validationData, testData)
 
 def editData():
-    td, vd, testd = loadData()
-    trainingInputs = [x for x in td[0]]
-    trainingResults = [vectorResult(y) for y in td[1]]
-    trainingData = zip(trainingInputs, trainingData)
-    return trainingData, vd, testd
+    tr_d, va_d, te_d = loadData()
+    training_inputs = [np.reshape(x, (784, 1)) for x in tr_d[0]]
+    training_results = [vectorResult(y) for y in tr_d[1]]
+    training_data = zip(training_inputs, training_results)
+    validation_inputs = [np.reshape(x, (784, 1)) for x in va_d[0]]
+    validation_data = zip(validation_inputs, va_d[1])
+    test_inputs = [np.reshape(x, (784, 1)) for x in te_d[0]]
+    test_data = zip(test_inputs, te_d[1])
+    return (training_data, validation_data, test_data)
+
+def vectorResult(i):
+    zeros = np.zeros((10, 1))
+    zeros[i] = 1.0
+    return zeros
